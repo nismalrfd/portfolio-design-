4d2f1489965e697757685b2ea4c4171ccfc0203b
nismal <nismalm14@gmail.com>
2022-11-20 15:05:53 +0530

    updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -49,4 +49,12 @@ function myFunction() {
   }
 }
 
+     let navbar = document.getElementById("nav-section");
+     let navOffset = navbar.offsetTop;
+     window.addEventListener("scroll", () => {
+         (window.scrollY >= navOffset) ? 
+             navbar.classList.add("fixed-nav") : 
+             navbar.classList.remove("fixed-nav")
+     });
+
 
